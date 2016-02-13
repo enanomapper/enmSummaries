2c01e1c6ae72dd3ff8be06dacce5e2d631ddbc73
Egon Willighagen <egon.willighagen@gmail.com>
2016-02-13 14:39:50 +0100

    Search materials based on a compound

--- src/Substance.js
+++ src/Substance.js
@@ -29,6 +29,28 @@ Ambit.Substance.prototype.list = function(callback) {
 }
 
 /**
+ * Lists all substances related to the given compound.
+ *
+ * @param {requestCallback} callback - Function that will be called with the result.
+ * @method
+ */
+Ambit.Substance.prototype.listForCompound = function(compound, callback) {
+	params = {};
+	params['compound_uri'] = compound;
+	params['type'] = 'related';
+	var conceptWikiSearcher = $.ajax({
+		url: this.baseURL + "/substance",
+		dataType: 'json',
+		success: function(response, status, request) {
+			callback.call(this, true, request.status, response);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
+
+/**
  * Search for substances.
  *
  * @param {string} query - The search query
